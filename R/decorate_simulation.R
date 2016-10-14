730ab2b5a4a668c325b7f9e15808dff562370cf1
Devin Pastoor <devin.pastoor@gmail.com>
2016-10-14 17:28:07 -0400

    rename to courier_msg

--- R/decorate_simulation.R
+++ R/decorate_simulation.R
@@ -3,7 +3,7 @@
 #' @param .port port send_message will communication on
 #' @details
 #' decorate_simulation will give the ability of the decorated function
-#' to use a function with a signature send_message(x) where
+#' to use a function with a signature courier_msg(x) where
 #' x is a string message. This message will be send via zmq
 #' to the receiving server. Client connection and teardown
 #' happen transparently
@@ -11,7 +11,7 @@
 #' @examples \dontrun{
 #'  PORT <- 5678 # number would be taken from run_receiving_server()
 #'  my_sim <- decorate_simulation(function(rep) {
-#'  send_message(paste("on rep", rep))
+#'  courier_msg(paste("on rep", rep))
 #'  # emulate 1 second of simulation 'work'
 #'  Sys.sleep(1)
 #'  return(runif(1, 0, 100))
@@ -21,7 +21,7 @@
 #' }
 decorate_simulation <- function(.f, .port) {
     .e <- new.env()
-    assign("send_message",
+    assign("courier_msg",
            function(x) {
               .client <- init_sink_client(.port)
               send_msg <- send_message_factory(.client)
