0532623d00674619a0860ec9b176deafb8d458be
Devin Pastoor <devin.pastoor@gmail.com>
2016-10-21 10:13:24 -0400

    r6 class to communicate with server

--- test_r6_class.R
+++ test_r6_class.R
@@ -4,22 +4,37 @@ Courier <- R6Class("courier",
        list(
          initialize = function(port) {
            print(paste("init with port", port))
+           context <- pbdZMQ::zmq.ctx.new()
+           sink <- pbdZMQ::zmq.socket(context, pbdZMQ::ZMQ.ST()$PUSH)
+           pbdZMQ::zmq.connect(sink,
+                               pbdZMQ::address("localhost",
+                                               port))
            private$port <<- port
+           private$context <<- context
+           private$client <<- sink
+           print(paste("successfully init with port", port))
          },
          finalize = function() {
            print("Finalizer has been called!")
+           pbdZMQ::zmq.close(private$client)
+           pbdZMQ::zmq.ctx.destroy(private$context)
+           print("done cleaning up!")
          },
-         send_msg = function(x) {
-           print("sending message:")
-          print(x)
+         send_msg = function(msg) {
+          print("sending message:")
+          print(msg)
+          pbdZMQ::zmq.send(private$client, msg)
+          invisible()
          }
        ),
      private =
        list(
-         port = NULL
+         port = NULL,
+         client = NULL,
+         context = NULL
        )
 )
-new_courier <- Courier$new(12)
-new_courier$send_msg("hello")
-rm(new_courier)
+msgr <- Courier$new(5555)
+msgr$send_msg("hello")
+rm(msgr)
 gc()
