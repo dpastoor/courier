9a210575a3d516601c96853b572ced801cf0b50c
Devin Pastoor <devin.pastoor@gmail.com>
2016-10-25 18:27:41 -0700

    refactor how to kill receiving server on a port and update docs

--- R/kill_receiving_server.R
+++ R/kill_receiving_server.R
@@ -1,11 +1,8 @@
-#' send a kill message to a listening receiving server
+#' send a kill message to a listening Receiver server
 #' @param .port port server is listening on
 #' @export
-kill_receiving_server <- function(.port) {
-    .client <- init_sink_client(.port)
-    send_msg <- send_message_factory(.client)
-    send_msg("__KILL__")
-    cleanup_zmq_list(.client)
-    message("kill request sent")
+kill_receiver_on_port <- function(.port) {
+    msgr <- Messenger$new(.port)
+    msgr$send_kill_msg()
     return(TRUE)
 }
